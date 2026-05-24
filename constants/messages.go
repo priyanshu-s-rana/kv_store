cfde723632666926e8712225e78e83ce74a06bae
priyanshu_s_rana <93531440+priyanshu-s-rana@users.noreply.github.com>
2026-05-24 21:25:01 +0530

    feat: made the parser for RESP and added its tests.

--- constants/messages.go
+++ constants/messages.go
@@ -8,7 +8,15 @@ const (
 	TTL_KEY_NOT_EXIST = ":-2\r\n"
 	OK                = "+OK\r\n"
 	PONG              = "+PONG\r\n"
-	INV_EXPIRY        = "-ERR invalid expiry time\r\n"
-	WRONG_NUM_ARGS    = "-ERR wrong number of arguments for %s\r\n"
-	UNKNOWN_CMD 	  = "-ERR unknown command %s\r\n"
+
+	// Error messages
+	INV_EXPIRY         = "-ERR invalid expiry time\r\n"
+	WRONG_NUM_ARGS     = "-ERR wrong number of arguments for %s\r\n"
+	UNKNOWN_CMD        = "-ERR unknown command %s\r\n"
+	INV_ARR_LEN_PARSER = "invalid array length: %s"
+	EMPTY_CMD          = "empty line command"
+	INV_ARRAY_LEN      = "invalid array length: %s"
+	INV_CMD_ARRAY_LEN  = "invalid command array length: %d"
+	INV_STR_PARSER     = "invalid bulk string format: %s"
+	INV_STR_LEN_PARSER = "invalid bulk string length: %s"
 )
